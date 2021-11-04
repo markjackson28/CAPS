e012c1978572205ef4c75777c67b6aa54f5b2239
Mark <mark28ten@gmail.com>
2021-11-04 14:45:10 -0700

    leftover files

--- __tests__/caps.test.js
+++ __tests__/caps.test.js
@@ -2,14 +2,17 @@
 
 const events = require('../src/event-pool');
 const faker = require('faker');
-const { event } = require('../src/caps');
-const { handleDelivery } = require('../src/utils/driver');
+const { event } = require('../caps');
+const { handleDelivery } = require('../src/utils/vendor');
+// const { handleDelivery } = require('../src/utils/vendor');
+// const { handleDelivery } = require('../src/utils/driver');
+require('../src/utils/driver')
 
 let consoleSpy;
 let mockPayload;
 
 beforeEach(() => {
-  consoleSpy = jestspyOn(console, 'log').mockImplementation()
+  consoleSpy = jest.spyOn(console, 'log').mockImplementation()
   mockPayload = {
     store: faker.company.companyName(),
     orderID: faker.datatype.uuid(),
@@ -18,8 +21,21 @@ beforeEach(() => {
   }
 });
 
+afterEach(() => {
+  consoleSpy.mockRestore();
+})
+
 describe('CAPS test', () => {
 
-  
+  it('consoles the pickup order ID', () => {
+    events.emit('pickup', mockPayload)
+    // handleDelivery(mockPayload)
+    expect(consoleSpy).toHaveBeenCalledWith(`DRIVER: picked up ${mockPayload.orderID}`);
+  })
+
+  it('consoles in-transit order ID', () => {
+    events.emit('in-transit', mockPayload)
+    expect(consoleSpy).toHaveBeenCalledWith(`EVENT, ${mockPayload}`)
+  });
 
 });
